d756c88d71ae160392f624dfa3546663a4046cdb
git-gio <giovanni.morlacchi@studenti.unimi.it>
2024-02-10 21:23:00 +0100

    work in progress create card and draw mechanics

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DeckManager : MonoBehaviour
+{
+    public List<Card> deck = new List<Card>();
+    // public Transform[] cardSlots;
+    public bool hasCard = false;
+
+    public GameObject rightHand;
+
+    public TMP_Text deckSizeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deckSizeText.text = "Deck Size: " + deck.Count.ToString();
+    }
+
+    public void DrawCards()
+    {
+        if (deck.Count == 0)
+        {
+            Debug.Log("No cards left");
+        }
+        else
+        {
+            Card randCard = deck[Random.Range(0, deck.Count)];
+            if (hasCard == false)
+            {
+                randCard.gameObject.SetActive(true);
+                randCard.transform.parent = rightHand.transform;
+                deck.Remove(randCard);
+                hasCard = true;
+                return;
+            }
+        }
+    }
+}
+
